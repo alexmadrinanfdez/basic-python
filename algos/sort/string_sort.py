27ff2950e1e9f04072b86e9295635d432d6e844e
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2022-01-07 17:37:41 -0600

    efficient string sorting strategy

---
+++ algos/sort/string_sort.py
@@ -0,0 +1,17 @@
+def _char_counts(string):
+    size = 256 # ASCII
+    cnts = [0] * size
+    for char in string.replace(' ', ''):
+        cnts[ord(char)] += 1
+    return cnts
+
+def string_sort(string):
+    cnts = _char_counts(string)
+    res = ""
+    for i, n in enumerate(cnts):
+        res += chr(i) * n
+    return res
+
+if __name__ == "__main__":
+    s = "the quick brown fox jumps over the lazy dog"
+    print(string_sort(s))
